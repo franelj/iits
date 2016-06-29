50ed98d59d0a06cd02096c8f96922587c9c1cb30
Aurélien <aurelien.prieur@epitech.eu>
2016-06-29 11:14:53 -0700

    skdjqs

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../lib/db');
+var check_params = ('../middlewares/check_parameters');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/authenticate', check_params(['username', 'password']), function(req, res, next) {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  res.send('Dev in progress');
 });
 
 module.exports = router;
