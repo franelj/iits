b097a3ea707edd99f8408c6d44de5cdf700ee9ab
Grégoire Lafitte <gregoirelafitte@Gregoires-MBP.csusm.edu>
2016-07-06 12:08:04 -0700

    add create reward route

--- routes/rewards.js
+++ routes/rewards.js
@@ -5,16 +5,21 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../lib/db.js');
+var errors = require('../lib/errors.js');
 var check_params = require('../middlewares/check_parameters');
-var multer = reauire('multer');
+var multer = require('multer');
+var upload = multer({ dest: './uploads/' });
 
-router.post('/', check_params(["name", "description", "points", "picture", "barcode"]), function(req, res, next) {
-
+router.post('/', upload.single('picture'), check_params(["name", "description", "points"]), function(req, res, next) {
+    db.query(`INSERT INTO rewards (name, description, points, picture) VALUES ("${req.body.name}", "${req.body.description}", "${req.body.points}", "${req.file.path}")`, function(err, rows, fields) {
+        if (err)
+            return next(new errors.DatabaseError('An error occurred when creating new reward'), req, res);
+    });
+    res.json({success: true});
 });
 
 /* GET reward listing. */
 router.get('/list', function(req, res, next) {
-
     res.send('respond with a resource');
 });
 
