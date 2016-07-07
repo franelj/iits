f702e2921b4244ae50f3229b3d0495f990ee2150
franelj <franeljulie@yahoo.fr>
2016-07-07 12:02:50 -0700

    Verify admin rights before creating an event

--- routes/events.js
+++ routes/events.js
@@ -5,14 +5,16 @@ var multer = require("multer");
 var upload = multer({ dest: './uploads/' });
 var db = require('../lib/db');
 var errors = require('../lib/errors');
+var user = require("../lib/users.js");
 
-router.post('/create', upload.single("picture"), check_parameters(["name", "description", "points"]), function(req, res, next) {
+router.post('/create', [user.authMiddleware, upload.single("picture"), check_parameters(["name", "description", "points"])], function(req, res, next) {
+    if (!user.isAdmin(req.currentUser)) {
+        return next(new errors.PermissionDeniedError("You do not have the rights to create an event"), req, res);
+    }
     db.query(`INSERT INTO events (name, description, points, picture) VALUES ("${req.body.name}", "${req.body.description}", "${req.body.points}", 
     "${req.file.path}")`, function(err, rows, fields) {
         if (err) {
-            return next(new errors.DatabaseError(
-                `An error occurred while updating the database`), req, res
-            );
+            return next(new errors.DatabaseError("An error occurred while updating the database"), req, res);
         }
         res.json({ success: true });
     });
