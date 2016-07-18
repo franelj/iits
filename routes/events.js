88be74f91fefb6b1f5e0eeb3d62e5d1eae81ed90
franelj <franeljulie@yahoo.fr>
2016-07-18 15:41:21 -0700

    Added barcode generation when event is created

--- routes/events.js
+++ routes/events.js
@@ -6,17 +6,31 @@ var upload = multer({ dest: './uploads/' });
 var db = require('../lib/db');
 var errors = require('../lib/errors');
 var user = require("../lib/users.js");
+var barcode = require("../lib/barcode");
 
-router.post('/create', [user.authMiddleware, upload.single("picture"), check_parameters(["name", "description", "points"])], function(req, res, next) {
-    if (!user.isAdmin(req.currentUser)) {
-        return next(new errors.PermissionDeniedError("You do not have the rights to create an event"), req, res);
-    }
-    db.query(`INSERT INTO events (name, description, points, picture) VALUES ("${req.body.name}", "${req.body.description}", "${req.body.points}", 
-    "${req.file.path}")`, function(err, rows, fields) {
+router.post('/create', [/*user.authMiddleware, */upload.single("picture"), check_parameters(["name", "description", "points"])], function(req, res, next) {
+    // if (!user.isAdmin(req.currentUser)) {
+    //     return next(new errors.PermissionDeniedError("You do not have the rights to create an event"), req, res);
+    // }
+    barcode.generateBarcodePng(function(err, png, code) {
         if (err) {
-            return next(new errors.DatabaseError("An error occurred while updating the database"), req, res);
+            return next(new errors.InvalidEntityError("Barcode invalid"));
+        } else {
+            db.query(`INSERT INTO events (name, description, points, picture, barcode) VALUES ("${req.body.name}", "${req.body.description}",
+                    "${req.body.points}", "${req.file.path}", "${code}")`, function(err, rows, fields) {
+                if (err) {
+                    return next(new errors.DatabaseError("An error occurred while updating the database"), req, res);
+                }
+                res.json({
+                    barcode: {
+                        picture: png,
+                        length: png.length,
+                        width: png.readUInt32BE(16),
+                        height: png.readUInt32BE(20)
+                    }
+                });
+            });
         }
-        res.json({ success: true });
     });
 });
 
