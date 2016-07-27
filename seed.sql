97f5683e580c8ff153332dd5c704bb8a33fcdcfc
Aurélien <aurelien.prieur@epitech.eu>
2016-07-27 11:24:12 -0700

    Trying to fix deploy issue with sql

--- seed.sql
+++ seed.sql
@@ -12,11 +12,11 @@ INSERT INTO users VALUES('', 'user-1', 'test', 0, 0, 0);
 INSERT INTO users VALUES('', 'admin', 'test', 0, 0, 0);
 INSERT INTO users VALUES('', 'superadmin', 'test', 0, 0, 0);
 
-INSERT INTO events VALUES('', 'test-1', 'Test event 1', '100');
-INSERT INTO events VALUES('', 'test-2', 'Test event 2', '200');
-INSERT INTO events VALUES('', 'test-3', 'Test event 3', '300');
-INSERT INTO events VALUES('', 'test-4', 'Test event 4', '50');
-INSERT INTO events VALUES('', 'test-5', 'Test event 5', '110');
+INSERT INTO events ('name', 'description', 'points') VALUES('test-1', 'Test event 1', '100');
+INSERT INTO events ('name', 'description', 'points') VALUES('test-2', 'Test event 2', '200');
+INSERT INTO events ('name', 'description', 'points') VALUES('test-3', 'Test event 3', '300');
+INSERT INTO events ('name', 'description', 'points') VALUES('test-4', 'Test event 4', '50');
+INSERT INTO events ('name', 'description', 'points') VALUES('test-5', 'Test event 5', '110');
 
-INSERT INTO rewards VALUES('', 'reward1', 'test reward 1', '50');
-INSERT INTO rewards VALUES('', 'reward2', 'test reward 2', '100');
+INSERT INTO rewards ('name', 'description', 'points') VALUES('reward1', 'test reward 1', '50');
+INSERT INTO rewards ('name', 'description', 'points') VALUES('reward2', 'test reward 2', '100');
