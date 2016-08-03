7f67794f0c7aca4b5469938c6907ffb4ab1e88c1
Grégoire Lafitte <gregoirelafitte@Gregoires-MBP.csusm.edu>
2016-08-03 11:38:34 -0700

    add points to admin

--- seed.sql
+++ seed.sql
@@ -9,7 +9,7 @@ SET FOREIGN_KEY_CHECKS = 1;
 
 
 INSERT INTO users VALUES('', 'user-1', 'test', 0, 0, 0);
-INSERT INTO users VALUES('', 'admin', 'test', 0, 1, 0);
+INSERT INTO users VALUES('', 'admin', 'test', 0, 1, 10000);
 INSERT INTO users VALUES('', 'superadmin', 'test', 0, 2, 0);
 
 INSERT INTO events (`name`, `description`, `points`, `date`) VALUES ('test-1', 'Test event 1', 100, NOW()), ('test-2', 'Test event 2', 200, NOW()),
